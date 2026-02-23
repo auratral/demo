09a2b06e8669d2673a91d23a8552ea81eca1d8c1
auratral <connect@auratral.com>
2026-02-23 19:58:39 +0530

    Remove base path configurations for root domain

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import Dashboard from './pages/Dashboard';
 
 function App() {
     return (
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <BrowserRouter>
             <div className="flex flex-col min-h-screen">
                 <Navbar />
 
